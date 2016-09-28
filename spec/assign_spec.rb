5bc7d670b93bdd0e3c71cf9a2c34330befae83c8
gameFu <223328084@qq.com>
2016-09-28 15:54:02 +0800

    assign variable test fix

--- spec/assign_spec.rb
+++ spec/assign_spec.rb
@@ -1,1 +1,14 @@
-require 'spec_help'
+require 'spec_helper'
+
+describe Assign do
+  # 赋值
+  it "assign" do
+    staement = Assign.new(:x, Add.new(Variable.new(:x), Number.new(1)))
+    env = {x: Number.new(2)}
+    expect(staement.reducible?).to eq true
+    staement, env = staement.reduce(env)
+    staement, env = staement.reduce(env)
+    staement, env = staement.reduce(env)
+    expect(env[:x].value).to eq 3
+  end
+end
--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 describe Variable do
   it "variable" do
-    num =  Machine.new(
-      Add.new( Variable.new(:x), Variable.new(:y)),
-      {x: Number.new(3), y: Number.new(4)}
-    ).run
+    num = Add.new( Variable.new(:x), Variable.new(:y)).reduce({x: Number.new(3), y: Number.new(4)}).reduce({x: Number.new(3), y: Number.new(4)}).reduce(({x: Number.new(3), y: Number.new(4)}))
     expect(num.value).to eq(7)
   end
 end
